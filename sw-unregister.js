e0adeaaf7d791717c2cb8bd5242221ca57a4e6d0
Tu Nombre <tu-email@email.com>
2026-03-11 13:54:51 -0400

    Force unregister old service workers

---
+++ sw-unregister.js
@@ -0,0 +1,8 @@
+// Desregistrar todos los service workers existentes
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+        registrations.forEach(registration => {
+            registration.unregister();
+        });
+    });
+}
